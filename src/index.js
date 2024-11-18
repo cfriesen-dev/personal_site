6b5585c17f3429b072e5283160e3b6288882ebef
cfriesen-dev <80776753+cfriesen-dev@users.noreply.github.com>
2024-11-18 10:09:02 -0800

    Updating personal site to react 18, react-router 6, and react-scripts 5

--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './App';
-import { HashRouter } from "react-router-dom";
+import { createHashRouter, RouterProvider } from "react-router-dom";
+import ErrorPage from "./error-page";
+import LandingPage from './landingPage/landingPage';
+import PhishingTest from './phishingTest/phishingTest';
+import Topic from './topic/topic'
 
-ReactDOM.render(
+
+const router = createHashRouter([{
+	path: "/",
+	element: <LandingPage />,
+	errorElement: <ErrorPage />,
+},{
+	path: "/phishing",
+	element: <Topic topic={'Phishing'} />,
+	errorElement: <ErrorPage />,
+},
+{
+	path: "/phishingTest",
+	element: <PhishingTest />,
+	errorElement: <ErrorPage />,
+},
+{
+	path: "/fingerprinting",
+	element: <Topic topic={'Fingerprinting'} />,
+	errorElement: <ErrorPage />,
+},
+{
+	path: "/anonymity",
+	element: <Topic topic={'Anonymity'} />,
+	errorElement: <ErrorPage />,
+},
+])
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
 	<React.StrictMode>
-		<HashRouter>
-			<App />
-		</HashRouter>
-	</React.StrictMode>,
-	document.getElementById('root'));
+		<RouterProvider router={router} />
+	</React.StrictMode>);
