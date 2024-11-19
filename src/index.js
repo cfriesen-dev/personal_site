32b55a838a4bed070ec6cb21feea32a697a66293
cfriesen-dev <80776753+cfriesen-dev@users.noreply.github.com>
2024-11-19 08:55:23 -0800

    Remove unused import

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 import { createHashRouter, RouterProvider } from "react-router-dom";
 import ErrorPage from "./error-page";
 import LandingPage from './landingPage/landingPage';
