0a1a149e7c0305f70637e2e6f6e3657d7cde1f7c
cfriesen-dev <80776753+cfriesen-dev@users.noreply.github.com>
2024-12-10 21:40:28 -0800

    Testing for topic content

--- src/topic/topic.spec.js
+++ src/topic/topic.spec.js
@@ -1,6 +1,7 @@
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import Topic from './topic'
 import { HashRouter } from "react-router-dom";
+import '@testing-library/jest-dom'
 
 it('renders all topics without crashing', () => {
     let TOPIC_DATA = require("./topics.json");
@@ -8,4 +9,9 @@ it('renders all topics without crashing', () => {
     for (let i = 0; i < TOPIC_DATA.topics.length; i++) {
         render(<Topic topic={TOPIC_DATA.topics[i].name} />, {wrapper: HashRouter});
     }
+  });
+
+  it('renders the appropriate topic content', () => {
+    render(<Topic topic={'Phishing'} />, {wrapper: HashRouter});
+    expect(screen.getByText(/Phishing is the attempt to gain sensitive information/i)).toBeInTheDocument()
   });
