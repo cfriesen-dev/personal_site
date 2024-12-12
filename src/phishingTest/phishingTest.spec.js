5fe6ffd5bcbd32aa0fde7dceef825380e79f72a3
cfriesen-dev <80776753+cfriesen-dev@users.noreply.github.com>
2024-12-11 22:06:13 -0800

    Add initial test for phishingTest

---
+++ src/phishingTest/phishingTest.spec.js
@@ -0,0 +1,8 @@
+import PhishingTest from "./phishingTest";
+import { HashRouter } from "react-router-dom";
+import { render } from '@testing-library/react'
+import '@testing-library/jest-dom'
+
+it('renders without crashing', () => {
+    render(<PhishingTest />, {wrapper: HashRouter});
+  });
