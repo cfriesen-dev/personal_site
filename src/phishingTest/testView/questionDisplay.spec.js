af4d30105f120ed85361d5235246efb7ea68de86
cfriesen-dev <80776753+cfriesen-dev@users.noreply.github.com>
2024-12-15 20:18:25 -0800

    test question display interaction functionality

--- src/phishingTest/testView/questionDisplay.spec.js
+++ src/phishingTest/testView/questionDisplay.spec.js
@@ -1,5 +1,5 @@
 import QuestionDisplay from "./questionDisplay";
-import { render } from '@testing-library/react'
+import { render, screen, fireEvent } from '@testing-library/react'
 import '@testing-library/jest-dom'
 
 it('renders without crashing', () => {
@@ -30,11 +30,76 @@ it('test data always displays valid answer', () => {
             render(<QuestionDisplay 
                 question={ question }
                 completeQuestion={ onCompleteMock }
-            />)
+            />);
 
             expect(screen.getByRole('button', {
                 name: new RegExp(question.answer, "i")
               })).toBeInTheDocument()
         }
     }
-})
+});
+
+it('includes default options', () => {
+    let onCompleteMock = jest.fn();
+
+    render(<QuestionDisplay 
+        question={
+            {
+                id: 0,
+                url: "https://microsoft.com",
+                destinations: [
+                    "Mobile's website", "Google's website"
+                ],
+                answer: "A website not listed"
+            }
+        }
+        completeQuestion={ onCompleteMock }/>);
+        
+        expect(screen.getByRole('button', { name: /A website not listed/i })).toBeInTheDocument()
+        expect(screen.getByRole('button', { name: /Redirects to another website/i })).toBeInTheDocument()
+});
+
+it('correctly identifies valid answers', () => {
+    let onCompleteMock = jest.fn();
+
+    render(<QuestionDisplay 
+        question={
+            {
+                id: 0,
+                url: "https://microsoft.com",
+                destinations: [
+                    "Mobile's website", "Google's website"
+                ],
+                answer: "A website not listed"
+            }
+        }
+        completeQuestion={ onCompleteMock }/>);
+        
+        const button = screen.getByRole('button', { name: /A website not listed/i })
+        fireEvent.click(button)
+
+        expect(onCompleteMock).toHaveBeenCalledWith(true);
+});
+
+it('correctly identifies invalid answers', () => {
+    let onCompleteMock = jest.fn();
+
+    render(<QuestionDisplay 
+        question={
+            {
+                id: 0,
+                url: "https://microsoft.com",
+                destinations: [
+                    "Mobile's website", "Google's website"
+                ],
+                answer: "A website not listed"
+            }
+        }
+        completeQuestion={ onCompleteMock }/>);
+        
+        const button = screen.getByRole('button', { name: /Mobile's website/i })
+        fireEvent.click(button)
+
+        expect(onCompleteMock).toHaveBeenCalledWith(false);
+});
+
