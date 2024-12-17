d5b46682190d3c5de6a25b85fab7dcfc44e840fa
cfriesen-dev <80776753+cfriesen-dev@users.noreply.github.com>
2024-12-16 22:07:26 -0800

    Test footer selection logic

--- src/phishingTest/phishingTest.spec.js
+++ src/phishingTest/phishingTest.spec.js
@@ -1,8 +1,13 @@
 import PhishingTest from "./phishingTest";
 import { HashRouter } from "react-router-dom";
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom'
 
 it('renders without crashing', () => {
     render(<PhishingTest />, {wrapper: HashRouter});
+});
+
+it('renders the correct footer', () => {
+    render(<PhishingTest />, {wrapper: HashRouter});
+    expect(screen.getByText(/TULiPS Research Group/i)).toBeInTheDocument()
 });
