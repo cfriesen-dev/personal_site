2b0fd3ec1b836b882aff96e2def78e55f9080570
cfriesen-dev <80776753+cfriesen-dev@users.noreply.github.com>
2025-03-02 18:44:46 -0700

    Add action list

--- src/privacyChecklist/actionList.jsx
+++ src/privacyChecklist/actionList.jsx
@@ -3,11 +3,33 @@ import PropTypes from "prop-types";
 
 export default function ActionList(props) {
   return (
-    <ol>
+    <ol classname="action_list">
       {props.check_list.map((area) => (
         <li className="area" key={area.category}>
           {area.category}
           <p>{area.description}</p>
+          <h4>Actions:</h4>
+          <ol>
+            {area.actions.map((action) => (
+              <li classname="action" key={action.title}>
+                {action.title}
+                <p>{action.condition}</p>
+                <h4>Steps:</h4>
+                <ol>
+                  {action.steps.map((step, index) => (
+                    <li classname="action-steps">
+                      <input
+                        type="checkbox"
+                        id={`${step}-${index}`}
+                        name={step}
+                      />
+                      <label for={`${step}-${index}`}>{step}</label>
+                    </li>
+                  ))}
+                </ol>
+              </li>
+            ))}
+          </ol>
         </li>
       ))}
     </ol>
